a3f8fac3726b04b62146c608b7cd30a683852a01
dduckcow <dduckcow_@naver.com>
2022-11-24 03:50:18 +0900

    temporary commit

--- OrbitElement.m
+++ OrbitElement.m
@@ -2,10 +2,10 @@ classdef OrbitElement
     properties
         semimajor
         eccentricity
-        inclination
-        right_ascension
-        argument_perigee
-        true_anomaly
+        inclination %Deg
+        right_ascension %Deg
+        argument_perigee %Deg
+        true_anomaly %Deg
     end
     methods
         function obj = OrbitElement(a, e, i, Sigma, omega, theta)
@@ -15,6 +15,56 @@ classdef OrbitElement
             obj.right_ascension = Sigma;
             obj.argument_perigee = omega;
             obj.true_anomaly = theta;
+        end
+
+        function theta = trueanomalyAfter(obj, tp)
+            arguments
+                obj (1, 1) OrbitElement
+                tp (1, 1) {mustBeReal, mustBeNonnegative}
+            end
+
+            global mu
+
+            theta0 = deg2rad(obj.true_anomaly);
+            a = obj.semimajor;
+            e = obj.eccentricity;
+            
+            E0 = 2*atan(tan(theta0/2) * sqrt((1-e)/(1+e))); % Eccentric Anomaly
+            M0 = (E0 - e*sin(E0)); % Mean Anomaly
+            M1 = M0 + sqrt(mu/a^2) * tp;
+            
+            % function define for Newton-Raphson method
+            f = @(E) M1 - (E - e*sin(E));
+            df = @(E) -(1 - e*cos(E));
+            E1 = NRmethod(E0, f, df);
+            
+            % true anoamly after tp
+            theta = 2*atan(tan(E1/2) * sqrt((1+e)/(1-e))); 
+        end
+
+        function r = pos(obj, tp)
+            arguments
+                obj (1, 1) OrbitElement
+                tp (1, 1) {mustBeReal, mustBeNonnegative}
+            end
+
+            a = obj.semimajor;
+            e = obj.eccentricity;
+            theta = obj.trueanomalyAfter(tp);
+            r = a*(1-e^2)/(1+e*cos(theta)) * [cos(theta), sin(theta)];
+        end
+
+        function v = vel(obj, tp)
+            arguments
+                obj (1, 1) OrbitElement
+                tp (1, 1) {mustBeReal, mustBeNonnegative}
+            end
+
+            global mu
+            a = obj.semimajor;
+            e = obj.eccentricity;
+            theta = obj.trueanomalyAfter(tp);
+            v = sqrt(mu/(a*(1-e^2))) * [-sin(theta), e+cos(theta)];
         end
     end
 end
--- multi_revolution.m
+++ multi_revolution.m
@@ -1,13 +1,10 @@
 clc; clear;
 close all;
 
-% au = 149597870.7; % 1AU (km)
-% mu = 398600 / (au^3); %km^3 s^-2 % The Earth
-
 global mu PLOT DEBUG
-% mu = 4*pi^2;
+mu = 3.986e5 * 3600; %km^3min^-2
 PLOT = false;
-DEBUG = false;
+DEBUG = true;
 
 % r1 = [1, 0, 0];
 % r2 = [0, 1.524, 0];
@@ -15,31 +12,39 @@ DEBUG = false;
 % lambmultifunc(r1, r2, tf);
 
 % Givens
+% km, deg
 Re = 6400;
-mu = 3.986e5;
-
 chaser = OrbitElement(Re+2000, 0.002, 60, 30, 0, 0);
 target = OrbitElement(Re+36000, 0.0002, 55, 35, -20, 30);
 
-t1 = 1200:10:1400;
-t2 = 1200:10:1400;
+% min
+t1 = 0:1:1400;
+t2 = 0:1:1400;
 result = zeros(length(t1), length(t2));
 for i = 1:length(t1)
     % calculate position vector, velocity vector
-    [r1, v1, theta1] = posvel(chaser.semimajor, chaser.eccentricity, chaser.true_anomaly, t1(i));
+    % [r1, v1, theta1] = posvel(chaser.semimajor, chaser.eccentricity, chaser.true_anomaly, t1(i));
+    r1 = chaser.pos(t1(i));
+    v1 = chaser.vel(t1(i));
+    theta1 = chaser.trueanomalyAfter(t1(i));
 
     for j = 1:length(t2)
-        [r2, v2, theta2] = posvel(target.semimajor, target.eccentricity, target.true_anomaly, t1(i)+t2(j));
+        % [r2, v2, theta2] = posvel(target.semimajor, target.eccentricity, target.true_anomaly, t1(i)+t2(j));
+        r2 = target.pos(t2(i));
+        v2 = target.vel(t2(i));
+        theta2 = target.trueanomalyAfter(t2(i));
+
         [maxN, A, E] = lambmultifunc(r1, r2, t2(j));
 
         J = Inf;
         for k = 1:length(A)
-            if (A(k) == 0)
+            if (A(k) == 0) || A(k) ~= real(A(k)) || E(k) ~= real(E(k))
                 continue;
             end
 
             vt1 = sqrt(mu/(A(k)*(1-E(k)^2))) * [-sin(theta1), E(k)+cos(theta1)];
             vt2 = sqrt(mu/(A(k)*(1-E(k)^2))) * [-sin(theta2), E(k)+cos(theta2)];
+            fprintf("vt1: %p, v1: %p\n", norm(vt1), norm(v1));
             
             dv1 = norm(vt1 - v1);
             dv2 = norm(v2 - vt2);
@@ -52,12 +57,16 @@ for i = 1:length(t1)
         end
 
         if J == Inf
-            J = -1;
+            J = 0;
         end
         result(i, j) = J;
         fprintf("i: %d, j: %d, J: %f\n", i, j, J);
     end
+    
 end
 
+save('result.mat','result');
+
+%%
 figure()
-contour(t1,t2,result);
+contour(t2, t1, result)
